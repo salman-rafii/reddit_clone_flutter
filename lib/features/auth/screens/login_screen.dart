f8999c10366b6ca64cce0384c2970978bd572a90
salman-rafii <salmanrafi125@gmail.com>
2023-01-15 12:13:09 +0500

    working on login screen basic UI

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone_flutter/core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.logoPath,
+          height: 40,
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
