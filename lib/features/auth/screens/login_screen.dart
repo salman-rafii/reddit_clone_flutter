2ac3bd91cdca8eb89b6f80f686ddedf5d094ab37
salman-rafii <salmanrafi125@gmail.com>
2023-01-18 00:07:05 +0500

    Firebase integration done

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone_flutter/core/common/sign_in_button.dart';
 import 'package:reddit_clone_flutter/core/constants/constants.dart';
+import 'package:reddit_clone_flutter/widgets/custom_text.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -14,6 +16,42 @@ class LoginScreen extends StatelessWidget {
           height: 40,
         ),
         centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const CustomText(
+              text: "Skip",
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            const CustomText(
+              text: "Dive into anything",
+              color: Colors.white,
+              fontSize: 24.0,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                Constants.loginEmotePath,
+                height: 400,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SignInButton()
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_clone_flutter/features/auth/screens/login_screen.dart';
 import 'package:reddit_clone_flutter/theme/pallete.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -13,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Reddit Clone',
       theme: Pallete.darkModeAppTheme,
       home: const LoginScreen(),
