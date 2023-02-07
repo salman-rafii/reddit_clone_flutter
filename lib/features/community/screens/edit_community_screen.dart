a410ab9fc6a8cd2b126d19e5826515133f9f34b9
salman-rafii <salmanrafi125@gmail.com>
2023-02-07 17:29:06 +0500

    edit community functionality done

--- lib/features/community/screens/edit_community_screen.dart
+++ lib/features/community/screens/edit_community_screen.dart
@@ -8,6 +8,7 @@ import 'package:reddit_clone_flutter/core/common/loader.dart';
 import 'package:reddit_clone_flutter/core/constants/constants.dart';
 import 'package:reddit_clone_flutter/core/utils.dart';
 import 'package:reddit_clone_flutter/features/community/controller/community_controller.dart';
+import 'package:reddit_clone_flutter/models/community_model.dart';
 import 'package:reddit_clone_flutter/theme/pallete.dart';
 import 'package:reddit_clone_flutter/widgets/custom_text.dart';
 
@@ -43,8 +44,18 @@ class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
     }
   }
 
+  void save(Community community) {
+    ref.read(communityControllerProvider.notifier).editCommunity(
+          community: community,
+          profileFile: profileFile,
+          bannerFile: bannerFile,
+          context: context,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
     return ref.watch(getCommunityByNameProvider(widget.name)).when(
           data: (community) => Scaffold(
             // backgroundColor: Pallete.darkModeAppTheme.colorScheme.background,
@@ -53,72 +64,77 @@ class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
               title: const CustomText(text: "Edit Community"),
               actions: [
                 TextButton(
-                    onPressed: () {}, child: const CustomText(text: "Save"))
+                    onPressed: () => save(community),
+                    child: const CustomText(text: "Save"))
               ],
             ),
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Padding(
+            body: isLoading
+                ? const Loader()
+                : Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      height: 200,
-                      child: Stack(
-                        children: [
-                          GestureDetector(
-                            onTap: selectBannerImage,
-                            child: DottedBorder(
-                              borderType: BorderType.RRect,
-                              dashPattern: const [10, 4],
-                              strokeCap: StrokeCap.round,
-                              radius: const Radius.circular(10),
-                              color: Pallete.darkModeAppTheme.textTheme
-                                  .bodyMedium!.color!,
-                              child: SizedBox(
-                                width: double.infinity,
-                                height: 150,
-                                child: bannerFile != null
-                                    ? Image.file(bannerFile!)
-                                    : community.banner.isEmpty ||
-                                            community.banner ==
-                                                Constants.bannerDefault
-                                        ? Center(
-                                            child: IconButton(
-                                                onPressed: () {},
-                                                icon: const Icon(
-                                                  Icons.camera_alt_outlined,
-                                                  size: 40,
-                                                )),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            height: 200,
+                            child: Stack(
+                              children: [
+                                GestureDetector(
+                                  onTap: selectBannerImage,
+                                  child: DottedBorder(
+                                    borderType: BorderType.RRect,
+                                    dashPattern: const [10, 4],
+                                    strokeCap: StrokeCap.round,
+                                    radius: const Radius.circular(10),
+                                    color: Pallete.darkModeAppTheme.textTheme
+                                        .bodyMedium!.color!,
+                                    child: SizedBox(
+                                      width: double.infinity,
+                                      height: 150,
+                                      child: bannerFile != null
+                                          ? Image.file(bannerFile!)
+                                          : community.banner.isEmpty ||
+                                                  community.banner ==
+                                                      Constants.bannerDefault
+                                              ? Center(
+                                                  child: IconButton(
+                                                      onPressed: () {},
+                                                      icon: const Icon(
+                                                        Icons
+                                                            .camera_alt_outlined,
+                                                        size: 40,
+                                                      )),
+                                                )
+                                              : Image.network(community.banner),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 20,
+                                  left: 20,
+                                  child: GestureDetector(
+                                    onTap: selectProfileImage,
+                                    child: profileFile != null
+                                        ? CircleAvatar(
+                                            backgroundImage:
+                                                FileImage(profileFile!),
+                                            radius: 32,
                                           )
-                                        : Image.network(community.banner),
-                              ),
+                                        : CircleAvatar(
+                                            backgroundImage:
+                                                NetworkImage(community.avatar),
+                                            radius: 32,
+                                          ),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
-                          Positioned(
-                            bottom: 20,
-                            left: 20,
-                            child: GestureDetector(
-                              onTap: selectProfileImage,
-                              child: profileFile != null
-                                  ? CircleAvatar(
-                                      backgroundImage: FileImage(profileFile!),
-                                      radius: 32,
-                                    )
-                                  : CircleAvatar(
-                                      backgroundImage:
-                                          NetworkImage(community.avatar),
-                                      radius: 32,
-                                    ),
-                            ),
-                          )
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
-            ),
+                  ),
           ),
           error: (error, stackTrace) => ErrorText(error: error.toString()),
           loading: () => const Loader(),
