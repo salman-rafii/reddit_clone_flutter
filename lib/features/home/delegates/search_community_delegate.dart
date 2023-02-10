b95b89b7808040a62eb008d9a6eed936bd892df5
salman-rafii <salmanrafi125@gmail.com>
2023-02-10 17:57:33 +0500

    working on searching communitites

--- lib/features/home/delegates/search_community_delegate.dart
+++ lib/features/home/delegates/search_community_delegate.dart
@@ -13,19 +13,16 @@ class SearchCommunityDelegate extends SearchDelegate {
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
     throw UnimplementedError();
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
     throw UnimplementedError();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
     throw UnimplementedError();
   }
 }
